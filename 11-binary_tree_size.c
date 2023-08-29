0ee6a241dbba3a6621d53801f6c7ada266b9b4c8
konditi1 <konditi1@gmail.com>
2023-08-30 01:32:26 +0300

    corrected betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -17,5 +17,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
 
-	return (left_size + right_size) + 1;
+	return ((left_size + right_size) + 1);
 }
