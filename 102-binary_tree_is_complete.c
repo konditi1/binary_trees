19e831918fa77464f856319f931513b8987c42a9
10thcode <attabadukwabena@gmail.com>
2023-09-01 12:30:45 +0000

    Fix 102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -10,11 +10,12 @@
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	int is_null_valid = 1;
+	size_t height = get_height(tree);
 
 	if (tree == NULL)
 		return (0);
 
-	return (is_complete(tree, &is_null_valid));
+	return (is_complete(tree, &is_null_valid, height));
 }
 
 /**
@@ -22,10 +23,11 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  *
  * @tree: the tree
  * @is_null_valid: check if right node can be null
+ * @height: the height of the tree
  *
  * Return: 1 if tree is complete, otherwise 0
  **/
-int is_complete(const binary_tree_t *tree, int *is_null_valid)
+int is_complete(const binary_tree_t *tree, int *is_null_valid, size_t height)
 {
 	if (tree == NULL)
 	{
@@ -39,15 +41,58 @@ int is_complete(const binary_tree_t *tree, int *is_null_valid)
 	}
 
 	if (*is_null_valid == 0)
-		return(0);
+		return (0);
 
 	if (tree->left == NULL)
 	{
 		if (tree->right == NULL)
-			return (1);
+			if (get_depth(tree) == height ||
+					get_depth(tree) + 1 == height)
+				return (1);
 		return (0);
 	}
 
-	return (is_complete(tree->left, is_null_valid) &&
-			is_complete(tree->right, is_null_valid));
+	return (is_complete(tree->left, is_null_valid, height) &&
+			is_complete(tree->right, is_null_valid, height));
+}
+
+/**
+ * get_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: a pointer to the node to measure the depth
+ *
+ * Return: the depth of a node
+ **/
+size_t get_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent == NULL)
+		return (0);
+
+	return (get_depth(tree->parent) + 1);
+}
+
+/**
+ * get_height - measures the height of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height of a binary tree
+ **/
+size_t get_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = get_height(tree->left);
+	right_height = get_height(tree->right);
+
+	return ((left_height > right_height ? left_height  + 1 : right_height + 1));
 }
